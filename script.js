f230328ba3d31cfbfe967c62af4f45eb6d0a35e6
ramama-codes25 <ramana.network24@gmail.com>
2026-03-24 13:29:21 +0530

    modify the send box

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
   
-const phone = "918056938174"; // no + for WhatsApp
-  const email = "ramana.network24@gmail.com";
+const phone = "918056938174"; 
+const email = "ramana.network24@gmail.com";
 
-  function callNow() {
-    window.location.href = "tel:+91" + phone.slice(-10);
+function callNow() {
+  window.location.href = "tel:+91" + phone.slice(-10);
+}
+
+function whatsappNow() {
+  window.location.href = "https://wa.me/" + phone;
+}
+
+function mailNow() {
+  window.location.href = "mailto:" + email;
+}
+
+function sendMessage() {
+
+  // Get values
+  const name = document.getElementById("name").value;
+  const userEmail = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  if (!name || !userEmail || !message) {
+    alert("Please fill all fields");
+    return;
   }
 
-  function whatsappNow() {
-    window.location.href = "https://wa.me/" + phone;
-  }
+  const text = encodeURIComponent(
+    `Name: ${name}\nEmail: ${userEmail}\nMessage: ${message}`
+  );
 
-  function mailNow() {
-    window.location.href = "mailto:" + email;
-
-    
-  }
+  const url = `https://wa.me/${phone}?text=${text}`;
+  window.open(url, "_blank");
+}
